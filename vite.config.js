92b706688cfcbfeb23f622172072e34753931a89
bbhhffdesign <bbhhff.design@gmail.com>
2025-01-27 18:28:23 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import VitePluginRestart from 'vite-plugin-restart'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
